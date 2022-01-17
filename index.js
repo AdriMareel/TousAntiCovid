460352e4d70f85c447060c9d4f7382e217ffa8a3
Eremith <loic.gdl2001@gmail.com>
2022-01-17 16:41:22 +0100

    tests

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ const mongoose = require('mongoose') // npm install mongoose
 const mongodb = 'mongodb+srv://admin:admin123@data.tr5qe.mongodb.net/covid_data?retryWrites=true&w=majority'
 //mongoose.connect(mongodb);
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(3000))
+    //.then((result) => app.listen(3000))
+    .then((result) => console.log("connected to database"))
     .catch((err) => console.log(err))
+
+app.use(express.static(__dirname + '/public'));
+//body parser
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.use('/', require('./routes/landing.js'))
+app.use('/users', require('./routes/users.js'))
+
+app.listen(3000)
