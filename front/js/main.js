de314b19561c8e7abf5858e55c7ff8bf737e5617
Eremith <loic.gdl2001@gmail.com>
2022-01-18 13:28:16 +0100

    login register and session token

--- front/js/main.js
+++ front/js/main.js
@@ -44,6 +44,35 @@ async function loginUser(event){
 
     if(result.status === 'ok'){
         console.log('Got the token: ', result.data)
+        localStorage.setItem('token', result.data)
+        console.log(localStorage, result.data)
+    } else {
+        alert(result.error)
+    }
+}
+
+
+
+let changeForm = document.getElementById('changeForm')
+if(changeForm) changeForm.addEventListener('submit', changePassword)
+
+async function changePassword(event){
+    event.preventDefault()
+    const password = document.getElementById('password').value
+
+    const result = await fetch('/change-password', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            newpassword: password,
+            token: localStorage.getItem('token')
+        })
+    }).then((res) => res.json())
+
+    if(result.status === 'ok'){
+        console.log('Successfully changed the password')
     } else {
         alert(result.error)
     }
