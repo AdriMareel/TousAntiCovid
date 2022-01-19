526fb359e16c386664c0a1c8875a520fb9d1e7f1
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2022-01-19 16:18:06 +0100

    no message

--- back/modules/toQRCode.js
+++ back/modules/toQRCode.js
@@ -1,11 +1,7 @@
 const QRCode = require('qrcode')
 
 function ToQRCode(id) {
-<<<<<<< HEAD
     let urlcomplete = "http://10.224.3.61:3000/VerifPasse?" + id;
-=======
-    let urlcomplete = "http://192.168.252.56:3000/VerifPasse?" + id;
->>>>>>> backEnd_Loïc
     let QRc = QRCode.toString(urlcomplete, function (err, url) {
     })
 
